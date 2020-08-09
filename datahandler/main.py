b0a301da740550947ade7dc495330d03b336eea1
JonathanSchubert <jonathanschubert@web.de>
2020-08-09 15:37:48 +0200

    write complete DB added to main.py

--- datahandler/main.py
+++ datahandler/main.py
@@ -15,11 +15,17 @@ def main():
     uba       = UBA()
     uba.create_history()
 
+    # # Manually write all data from files to DB
+    # netatmo.write_complete_history_to_db()
+    # luftdaten.write_complete_history_to_db()
+    # uba.write_complete_history_to_db()
+
     while True:
         uba.update_latest_data()
         netatmo.update_latest_data()
         luftdaten.update_latest_data()
 
         time.sleep(60)
+        print('sleep 60s...')
 
 main()
